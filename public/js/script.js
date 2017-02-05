ce513c0d1f4dd08d54be0c2e611b98408144bdee
Rishabh Bhatia <Rishabh Bhatia>
2017-02-06 04:55:32 +0530

    routes and views

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
 $.ajax({
 	url: "/api/pages",
 	    success: function( results ) {
-	    console.log(result)
 	    for(i=0 ; i < results.length ; i++){
 	    	var result = results[i];
 	    	$('#page_data').append('<tr><td>'+result.page_name+'</td>'+'<td>'+result.page_title+'</td>'+'<td>'+result.myslug+'</td></tr>');
@@ -13,9 +12,9 @@ $.ajax({
 $.ajax({
 	url: "/api/products",
 	success: function( products ){
+		console.log(products)
  		for(i=0; i < products.length ; i++){
  			var product = products[i];
- 			console.log(products)
  			$('#page_data').append('<tr><td>'+product.product_name+'</td>'+'<td>'+product.brand+'</td>'+'<td>'+product.category+'</td>'+'<td>'+product.discription+'</td>'+'<td>'+product.myslug+'</td>'+'<td>'+product.url+'</td></tr>')
  		}
 	}
