65152aef469a1e26ede4f19014fcee92aee0f556
DESKTOP-PCGN018\risha <irishabh96@gmail.com>
2017-03-05 00:40:51 +0530

    paytm.js

--- routes/scrape/paytm.js
+++ routes/scrape/paytm.js
@@ -16,26 +16,26 @@ function myTrim(x) {
 var currentprice;
 
 product.find({"websites": { $elemMatch: {name: 'amazon'} } }, {websites:1, _id:0}, function(err, result) {
-// 	// console.log(result)
 	for (var i = result.length - 1; i >= 0; i--) {
 		// console.log(result[i])
-		var websites = result.websites
-		// console.log(result[i].websites[0].url)
-		// console.log(result[i].websites[0]._id)
-		
-		console.log(result[i].websites)
+		// var websites = result.websites
+		console.log(result[i].websites[0].url)
+		console.log(result[i].websites[0]._id)
 	}
+
 })
 
+var urls = [
+	'https://paytm.com/shop/p/apple-iphone-5s-16-gb-space-grey-MOBAPPLE-IPHONEHAND209033D861E5F?src=search-grid&tracker=organic%7C66781%7Ciphone%205s%7Cgrid%7CSearch%7C%7C2%7Cproduction&site_id=1&child_site_id=1',
+	'https://paytm.com/shop/p/apple-iphone-5s-16-gb-silver-MOBAPPLE-IPHONENEWG19741390DB1436?src=search-grid&tracker=organic%7C66781%7Ciphone%205s%7Cgrid%7CSearch%7C%7C1%7Cproduction&site_id=1&child_site_id=1'
+]
 
-
-
-
-x('https://paytm.com/shop/p/apple-iphone-5s-16-gb-silver-MOBAPPLE-IPHONENEWG19741390DB1436?src=search-grid&tracker=organic%7C66781%7Ciphone%205s%7Cgrid%7CSearch%7C%7C1%7Cproduction&site_id=1&child_site_id=1', '._1d5g')(function(err, result){
-  
-  	currentprice = myTrim(result);
-  	console.log(currentprice)
-  	
+urls.forEach(function(url, index){
+  //whatever you need to do to prep your call to x
+ 	x(url, '._1d5g')(function(err, result){
+ 		// console.log(result)
+ 		currentprice = myTrim(result);
+  	console.log(currentprice)  	
 	price.create(
 		{
 			name: 'snapdeal',
@@ -56,5 +56,5 @@ x('https://paytm.com/shop/p/apple-iphone-5s-16-gb-silver-MOBAPPLE-IPHONENEWG1974
 				}
 			})
 
-});
-
+ 	})
+})
